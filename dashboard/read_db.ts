55614625bc19bb2fe9f6d11e0457ec4f35ec1ebf
Nicolas France <nicolasfrance01@users.noreply.github.com>
2026-09-10 15:25:29 -0300

    remove test files

--- dashboard/read_db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function run() {
-    const s = await prisma.syncHistory.findMany({ orderBy: { id: 'desc' }, take: 5 });
-    console.log(JSON.stringify(s, null, 2));
-}
-
-run().finally(() => prisma.$disconnect());
